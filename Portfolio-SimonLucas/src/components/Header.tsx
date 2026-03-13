e4669dcb80d123a5e9efe716732316ed974078db
Shinemi <simon.lucas.83136@gmail.com>
2026-03-13 14:32:54 +0100

    Refactor Header component to include logo and navigation structure

--- Portfolio-SimonLucas/src/components/Header.tsx
+++ Portfolio-SimonLucas/src/components/Header.tsx
@@ -1,5 +1,6 @@
 // components/Header.tsx
 import { Link } from 'react-router-dom';
+import logo from '../images/mockingBirdLogoNeon.png';
 
 interface Props {
   onContactOpen: () => void;
@@ -7,8 +8,27 @@ interface Props {
 
 export default function Header({ onContactOpen }: Props) {
   return (
-    <header>
-      {/* ta nav ici */}
+    <header className="header">
+
+      {/* Logo — renvoie vers la page d'accueil */}
+      <Link to="/" className="header-logo">
+        <img src={logo} alt="logo Neon"/>
+        ✦ PORTFOLIO
+      </Link>
+
+      {/* Navigation */}
+      <nav className="header-nav">
+        <Link to="/#langages">Langages</Link>
+        <Link to="/#projets">Projets</Link>
+        <Link to="/#skills">Compétences</Link>
+        <Link to="/projects">Tous les projets</Link>
+
+        {/* Bouton contact — ouvre la modale, pas une redirection */}
+        <button className="header-contact-btn" onClick={onContactOpen}>
+          Contact
+        </button>
+      </nav>
+
     </header>
   );
 }
