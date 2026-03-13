6610c4f0513a238f64b00f690b965272b7126544
Shinemi <simon.lucas.83136@gmail.com>
2026-03-13 15:15:40 +0100

    Update package dependencies and enhance Footer component with social links

--- Portfolio-SimonLucas/src/components/footer.tsx
+++ Portfolio-SimonLucas/src/components/footer.tsx
@@ -1,9 +1,20 @@
 // components/Footer.tsx
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer>
-      {/* ton footer ici */}
+      <p>&copy; 2026 Mon Portfolio. Tous droits réservés.</p>
+      <p>Dev & Art - Fait à la main</p>
+      <div>
+        <a href="https://www.linkedin.com/in/lucas-simon-7364382b7/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/LucasSimon">
+          <FaGithub />
+        </a>
+      </div>
     </footer>
   );
 }
